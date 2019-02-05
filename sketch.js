b4c229fa1beb40a39dd45564f093a756c11870df
SOURAVMONDAL007 <shreesouravkat@gmail.com>
2019-02-05 22:00:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
+var weather;
 function setup() {
   createCanvas(640, 480);
+  var API_key="bc79a33d1c8671364632e1ff075eb5da";
+  var str=""chicago";
+  loadJSON("https://api.openweathermap.org/data/2.5/weather?q="+str+"&appid="+API_key,gotData);
 }
-
+function gotData(data){
+  weather=data;
+}
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  if(weather){
+    ellipse(50,50,weather.data.temp, weather.data.temp);
+    ellipse(50,50,weather.data.humidity, weather.data.humidity);
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
