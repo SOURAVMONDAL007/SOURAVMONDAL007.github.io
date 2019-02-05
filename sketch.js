3e26bc99f357f07e94120573ade5de6956a93e01
SOURAVMONDAL007 <shreesouravkat@gmail.com>
2019-02-05 22:04:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ function setup() {
   createCanvas(640, 480);
   var API_key="bc79a33d1c8671364632e1ff075eb5da";
   var str="chicago";
-  loadJSON("https://api.openweathermap.org/data/2.5/weather?q="+str+"&appid="+API_key,gotData);
+  var API_callfunc="https://api.openweathermap.org/data/2.5/weather?q="+str+"&appid="+API_key;
+  loadJSON(API_callfunc,gotData);
 }
 function gotData(data){
   weather=data;
