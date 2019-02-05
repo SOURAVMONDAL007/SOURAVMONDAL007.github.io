cc295611836a1ca0c64ab860422d179eb08e677a
SOURAVMONDAL007 <shreesouravkat@gmail.com>
2019-02-05 23:06:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,13 @@
-var weather;
-function setup() {
-  createCanvas(640, 480);
+function report(){
   var API_key="bc79a33d1c8671364632e1ff075eb5da";
-  var str="chicago";
-  var API_callfunc="https://api.openweathermap.org/data/2.5/weather?q="+str+"&appid="+API_key;
-  loadJSON(API_callfunc,gotData);
+  var str=document.getElementById("report").elements["place"].value;
+  var API_callfunc="http://api.openweathermap.org/data/2.5/weather?q="+str+"&appid="+API_key;
+
+  $.getJSON(API_callfunc, function(result){
+      console.log(result);
+      document.getElementById("temp").innerHTML = "temperature\t"+result.main.temp;
+      document.getElementById("lat").innerHTML = "latitude\t"+result.coord.lat;
+      document.getElementById("lon").innerHTML = "longitude\t"+result.coord.lon;      
+  });
 }
-function gotData(data){
-  weather=data;
-}
-function draw() {
-  background(0);
-  if(weather){
-    ellipse(50,50,weather.data.temp, weather.data.temp);
-    ellipse(50,50,weather.data.humidity, weather.data.humidity);
-  }
-}
+
